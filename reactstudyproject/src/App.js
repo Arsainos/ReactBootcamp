0eb279845d3418b5dec6cef6203abe755cc65fbc
Belyavtsev Artyem <arsainos@gmail.com>
2019-07-09 21:38:22 +0300

    add first args to the person component

--- reactstudyproject/src/App.js
+++ reactstudyproject/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Hello world</h1>
         <p>Nice to meet you!</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My hobbies: guitar</Person>
         <Person />
       </div>
     );
