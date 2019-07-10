e7d4832c8a870b38bbee8892ce14e45c9cde0da0
Belyavtsev Artyem <arsainos@gmail.com>
2019-07-10 22:10:20 +0300

    hooks. passing methods between components

--- reactstudyproject/src/App.js
+++ reactstudyproject/src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello world</h1>
-        <p>Nice to meet you!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My hobbies: guitar</Person>
-        <Person />
-      </div>
-    );
-  }
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+      persons: [
+        { name: 'Max', age: 28},
+        { name: 'Manu', age: 29}
+      ],
+      otherState: 'Azaza'
+    }
+  );
+
+  const switchNameHandler = (newName) => {
+    //console.log('Was clicked!');
+    //DONT DO THIS WAY this.state.persons[0].name = 'Azazaelo';
+    setPersonsState({
+      persons: [
+      { name: newName, age: 18},
+      { name: 'Manu', age: 29}
+    ]})
+  };
+  
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>Nice to meet you!</p>
+      <button onClick={switchNameHandler}>Switch Name </button>
+      <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age}
+        click={switchNameHandler}
+      />
+      <Person name="Manu" age="29">My hobbies: guitar</Person>
+      <Person />
+    </div>
+  );
 }
 
+
 export default App;
+
+
+
