dad66fc4c2fe250361d21a5aab3cf3c50547041f
Belyavtsev Artyem <arsainos@gmail.com>
2019-07-11 21:07:40 +0300

    complete

--- reactstudyproject/src/App.js
+++ reactstudyproject/src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Person from './Person/Person';
 
 const App = props => {
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px'
+  }
+
   const [personsState, setPersonsState] = useState({
       persons: [
         { name: 'Max', age: 28},
@@ -11,6 +18,16 @@ const App = props => {
       otherState: 'Azaza'
     }
   );
+
+  const nameChangedHandler = (event) => {
+    //console.log('Was clicked!');
+    //DONT DO THIS WAY this.state.persons[0].name = 'Azazaelo';
+    setPersonsState({
+      persons: [
+      { name: event.target.value, age: 18},
+      { name: 'Manu', age: 29}
+    ]})
+  };
 
   const switchNameHandler = (newName) => {
     //console.log('Was clicked!');
@@ -26,11 +43,14 @@ const App = props => {
     <div className="App">
       <h1>Hello world</h1>
       <p>Nice to meet you!</p>
-      <button onClick={switchNameHandler}>Switch Name </button>
+      <button 
+      style={style}
+      onClick={switchNameHandler.bind(this,'Azaza')}>Switch Name </button>
       <Person 
         name={personsState.persons[0].name} 
         age={personsState.persons[0].age}
-        click={switchNameHandler}
+        click={switchNameHandler.bind(this, 'Rita')}
+        changed={nameChangedHandler}
       />
       <Person name="Manu" age="29">My hobbies: guitar</Person>
       <Person />
