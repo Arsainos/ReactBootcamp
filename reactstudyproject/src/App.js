c22a1d68f5f13c1be4ca19ede681c0771757e968
Belyavtsev Artyem <arsainos@gmail.com>
2019-07-11 21:59:21 +0300

    change back to the component extends

--- reactstudyproject/src/App.js
+++ reactstudyproject/src/App.js
@@ -1,61 +1,67 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const style = {
+class App extends Component {
+  style = {
     backgroundColor: 'white',
     font: 'inherit',
     border: '1px solid blue',
     padding: '8px'
   }
 
-  const [personsState, setPersonsState] = useState({
+  state = {
       persons: [
         { name: 'Max', age: 28},
         { name: 'Manu', age: 29}
       ],
       otherState: 'Azaza'
-    }
-  );
+    };
 
-  const nameChangedHandler = (event) => {
+  nameChangedHandler = (event) => {
     //console.log('Was clicked!');
     //DONT DO THIS WAY this.state.persons[0].name = 'Azazaelo';
-    setPersonsState({
+    this.setState({
       persons: [
       { name: event.target.value, age: 18},
       { name: 'Manu', age: 29}
     ]})
   };
 
-  const switchNameHandler = (newName) => {
+  switchNameHandler = (newName) => {
     //console.log('Was clicked!');
     //DONT DO THIS WAY this.state.persons[0].name = 'Azazaelo';
-    setPersonsState({
+    this.setState({
       persons: [
       { name: newName, age: 18},
       { name: 'Manu', age: 29}
     ]})
   };
-  
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <p>Nice to meet you!</p>
-      <button 
-      style={style}
-      onClick={switchNameHandler.bind(this,'Azaza')}>Switch Name </button>
-      <Person 
-        name={personsState.persons[0].name} 
-        age={personsState.persons[0].age}
-        click={switchNameHandler.bind(this, 'Rita')}
-        changed={nameChangedHandler}
-      />
-      <Person name="Manu" age="29">My hobbies: guitar</Person>
-      <Person />
-    </div>
-  );
+
+  tooglePersonHandler = () => {
+
+  };
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+        <p>Nice to meet you!</p>
+        <button 
+        style={style}
+        onClick={this.toglePersonHandler}>Switch Name </button>
+        <div >
+        <Person 
+            name={this.persons[0].name} 
+            age={this.persons[0].age}
+            click={this.switchNameHandler.bind(this, 'Rita')}
+            changed={this.nameChangedHandler}
+          />
+          <Person name="Manu" age="29">My hobbies: guitar</Person>
+          <Person />
+        </div>
+      </div>
+    )
+  }
 }
 
 
