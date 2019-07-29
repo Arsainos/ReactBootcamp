2492e24a0da8637dcdb9dffe024d6e231c03f7a1
Belyavtsev Artyem <arsainos@gmail.com>
2019-07-29 19:11:35 +0300

    first route component

--- httpRequests/src/containers/Blog/Blog.js
+++ httpRequests/src/containers/Blog/Blog.js
@@ -1,52 +1,12 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from './FullPost/FullPost';
-import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import { Route } from 'react-router-dom';
+
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-    
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Temych'
-                }
-            })
-            this.setState({posts: updatedPosts})
-            //console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-            this.setState({error: true})
-        });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    };
-    
+   
     render () {
-        let posts = <p style={{textAlign: 'center'}}> Something went wrong</p>;
-        if(!this.state.error){
-            posts = this.state.posts.map(post => {
-                return <Post key={post.id} 
-                author={post.author}
-                clicked={()=>this.postSelectedHandler(post.id)}
-                title={post.title}/>
-            })
-        }      
-
         return (
             <div className="Blog">
                 <header>
@@ -57,6 +17,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
+                {//<Route path="/" exact render={()=> <h1>HOME</h1>} /> 
+                }
+
+                <Route path="/" exact component={Posts} />
             </div>
         );
     }
---
+++ httpRequests/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Temych'
+                }
+            })
+            this.setState({posts: updatedPosts})
+            //console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+            //this.setState({error: true})
+        });
+    }
+
+    render() {
+        let posts = <p style={{textAlign: 'center'}}> Something went wrong</p>;
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post key={post.id} 
+                author={post.author}
+                clicked={()=>this.postSelectedHandler(post.id)}
+                title={post.title}/>
+            })
+        }  
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts;
