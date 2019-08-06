3e3b8e5f890412409feaeed427a9758c4648b41c
Belyavtsev Artyem <arsainos@gmail.com>
2019-08-06 16:54:35 +0300

    moving router through the router

--- httpRequests/src/containers/Blog/Blog.js
+++ httpRequests/src/containers/Blog/Blog.js
@@ -3,8 +3,13 @@ import './Blog.css';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from './NewPost/NewPost';
+//import FullPost from './FullPost/FullPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
    state = {
@@ -32,7 +37,7 @@ class Blog extends Component {
                 {//<Route path="/" exact render={()=> <h1>HOME</h1>} /> 
                 }
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts" exact component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
