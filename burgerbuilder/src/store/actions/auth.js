88e83fe02150b1d880d756e1403d43145c00bcd0
Belyavtsev Artyem <arsainos@gmail.com>
2019-11-05 21:12:42 +0300

    logout function and automatic logout after token have been expired

--- burgerbuilder/src/store/actions/auth.js
+++ burgerbuilder/src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime);
+    };
+};
+
 export const auth = (email, password, isSingup) => {
     return dispatch => {
         dispatch(authStart());
@@ -38,6 +52,7 @@ export const auth = (email, password, isSingup) => {
         axios.post(url, authData)
             .then(response => {
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(error => {
                 dispatch(authFail(error.response.data.error));
