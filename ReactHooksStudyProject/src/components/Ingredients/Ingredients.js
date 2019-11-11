b4f9d4972cd7718a73068a6471ac478366cc5780
Belyavtsev Artyem <arsainos@gmail.com>
2019-11-11 19:29:27 +0300

    add fetch for firebase with useEffect

--- ReactHooksStudyProject/src/components/Ingredients/Ingredients.js
+++ ReactHooksStudyProject/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,11 +7,42 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch('https://react-hooks-update-cad90.firebaseio.com/ingridients.json')
+    .then(response => response.json()
+    ).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData)
+      {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        })
+      }
+      //setUserIngredients(loadedIngredients);
+    })
+  }, []);
+
+  useEffect(() => {
+    console.log('Rendering ingredients');
+  });
+
+
+
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients, 
-      { id: Math.random().toString(), ...ingredient}
-    ]);
+    fetch('https://react-hooks-update-cad90.firebaseio.com/ingridients.json',{
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients, 
+        { id: responseData.name, ...ingredient}
+      ]);
+    })
   }
 
   const removeIngredientHandler = id => {
